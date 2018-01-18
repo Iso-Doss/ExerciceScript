6840813cdf668be3f6314e4b46b6d11ba30c5897
Iso-Doss <dossou.israel48@gmail.com>
2018-01-18 19:24:00 +0100

    Version stable avec 90% des fonctionnalité

--- langstat.sh
+++ langstat.sh
@@ -1,56 +1,29 @@
 #!/bin/bash
 
-# Vérification des paramètres
-# S'ils sont absents, on met une valeur par défaut
-
-sourceScript=`pwd`
-
+#Vérification de la présence du paramètre indiquant le nom du fichier dictionnaire à utiliser
 if [ -z $1 ]
 then
-        sortie='galerie.html'
+	echo "Parametre manquant. veuillez réesayer en saisissant un bon paramètre"
+	exit
 else
-        sortie=$1
+	fichier=$1
 fi
-if [ -z $2 ]
+
+#Vérification que le fichier dictionnaire existe bel et bien
+if [ ! -e $fichier ] || [ ! -f $fichier ]
 then
-        source='./'
-else
-        source=$2
+	echo "Parametre introuvable. Veuillez réesayer"
+	exit
 fi
 
-if [ -z $3 ]
-then
-        taille='200x200>'
-else
-        taille=$3
-fi
-
-# Préparation des fichiers et dossiers
-echo '' > $sourceScript/$sortie
-
-if [ ! -e miniatures ]
-then
-        mkdir miniatures
-fi
-
-# En-tête HTML
-cat $sourceScript/En-tête >> $sourceScript/$sortie
-
-# Génération des miniatures et de la page
-cd $source
-for image in `ls *.png *.jpg *.jpeg *.gif 2>/dev/null`
+#Boucle qui compte pour chaque lettre de l'alphabet le nombre d'occurence dans le fichier puis redirection de chaque résultats dans un fichier langstat.txt
+for lettre in 'A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z'
 do
-	date=`ls -l $image | cut -d ' ' -f 8 && ls -l $image | cut -d ' ' -f 6 && ls -l $image | cut -d ' ' -f 9`
-        convert $image -thumbnail $taille "$sourceScript/miniatures/$image"
-        echo '<div class="col-xs-4 col-sm-3 col-md-2 text-center"> <a href="'$sourceScript/$source/$image'"><img src="'$sourceScript/miniatures/$image'" alt="" class="img-rounded"></a><p>' $image '</p><p>Dernière modification:' $date '</p></div>' >> $sourceScript/$sortie
+	test=`grep -o $lettre $fichier | wc -l`
+	echo $test ' - ' $lettre >> langstat.txt
 done
 
-
-# Pied de page HTML
-cat $sourceScript/Pied-de-page >> $sourceScript/$sortie
-
-
-
-
-test=`grep -o / $sourceScript/Pied-de-page | wc -l`
-echo $test
+#Trie du fichier contenant les statistique sur chaque lettre
+#Affichage du nombre de fois que le fichier est utilisée
+#Puis suppression des fichier temporaire de travail sur le disque dur
+sort -rn langstat.txt > langstat ; cat langstat ; rm langstat langstat.txt
